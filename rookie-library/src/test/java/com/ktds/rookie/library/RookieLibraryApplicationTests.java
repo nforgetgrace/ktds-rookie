a625c52c498fa6a7a660c16a7a08f355d9119feb
nforgetgrace <nforgetgrace@gmail.com>
2023-04-06 00:08:15 +0900

    프로젝트 셋팅

---
+++ rookie-library/src/test/java/com/ktds/rookie/library/RookieLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktds.rookie.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RookieLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
